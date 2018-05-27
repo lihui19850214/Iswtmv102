17355e3a5d5c24a16354ccfa94dcffa8b31a4ad2
李辉 <>
2018-05-28 00:13:44 +0800

    未完成，但是提交后报错

--- src/com/icomp/Iswtmv10/v01c01/c01s024/C01S024_001Activity.java
+++ src/com/icomp/Iswtmv10/v01c01/c01s024/C01S024_001Activity.java
@@ -12,10 +12,13 @@ import android.widget.*;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.apiclient.pojo.AuthCustomer;
 import com.apiclient.pojo.CuttingToolBind;
-import com.apiclient.vo.CuttingToolBindVO;
-import com.apiclient.vo.RfidContainerVO;
-import com.apiclient.vo.SharpenVO;
+import com.apiclient.pojo.ProductLineEquipment;
+import com.apiclient.pojo.SynthesisCuttingToolBind;
+import com.apiclient.vo.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.icomp.Iswtmv10.R;
 import com.icomp.Iswtmv10.internet.IRequest;
@@ -24,6 +27,7 @@ import com.icomp.Iswtmv10.internet.RetrofitSingle;
 import com.icomp.Iswtmv10.v01c01.c01s018.C01S018_002Activity;
 import com.icomp.common.activity.CommonActivity;
 import com.icomp.common.utils.SysApplication;
+import com.icomp.entity.base.Equipment;
 import com.icomp.wsdl.v01c01.c01s024.C01S024Wsdl;
 import com.icomp.wsdl.v01c01.c01s024.endpoint.C01S024Request;
 import com.icomp.wsdl.v01c01.c01s024.endpoint.C01S024Respons;
@@ -31,6 +35,8 @@ import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+
+import java.text.SimpleDateFormat;
 
 /**
  * 快速查询页面1
@@ -122,7 +128,7 @@ public class C01S024_001Activity extends CommonActivity {
 
     //点击扫描按钮的方法
     private void scan() {
-        if (rfidWithUHF.startInventoryTag((byte) 0, (byte) 0)) {
+//        if (rfidWithUHF.startInventoryTag((byte) 0, (byte) 0)) {
             isCanScan = false;
             //点击扫描按钮以后，设置扫描按钮不可用
             btnScan.setClickable(false);
@@ -132,9 +138,9 @@ public class C01S024_001Activity extends CommonActivity {
             //开启扫描线程
             scanThread = new scanThread();
             scanThread.start();
-        } else {
-            Toast.makeText(getApplicationContext(), getString(R.string.initFail), Toast.LENGTH_SHORT).show();
-        }
+//        } else {
+//            Toast.makeText(getApplicationContext(), getString(R.string.initFail), Toast.LENGTH_SHORT).show();
+//        }
     }
 
     //扫描线程
@@ -143,8 +149,8 @@ public class C01S024_001Activity extends CommonActivity {
         public void run() {
             super.run();
             //调用单扫方法
-            rfidString = singleScan();
-
+//            rfidString = singleScan();
+rfidString="18000A00000D6440";
             if ("close".equals(rfidString)) {
                 btnScan.setClickable(true);
                 btnCancel.setClickable(true);
@@ -170,37 +176,33 @@ public class C01S024_001Activity extends CommonActivity {
                 //调用接口，查询合成刀具组成信息
                 IRequest iRequest = retrofit.create(IRequest.class);
 
-                RfidContainerVO rfidContainerVO = new RfidContainerVO();
-                rfidContainerVO.setLaserCode(rfidString);
+                FastQueryVO fastQueryVO = new FastQueryVO();
+                fastQueryVO.setRfidLaserCode(rfidString);
 
-                CuttingToolBindVO cuttingToolBindVO = new CuttingToolBindVO();
-                cuttingToolBindVO.setRfidContainerVO(rfidContainerVO);
+                final ObjectMapper mapper = new ObjectMapper();
 
-                Gson gson = new Gson();
-                String jsonStr = gson.toJson(cuttingToolBindVO);
+                String jsonStr = "";
+                try {
+                    jsonStr = mapper.writeValueAsString(fastQueryVO);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+
                 RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), jsonStr);
 
-                Call<String> getInCuttingToolBind = iRequest.getInCuttingToolBind(body);
-                getInCuttingToolBind.enqueue(new MyCallBack<String>() {
+                Call<String> fastQuery = iRequest.fastQuery(body);
+                fastQuery.enqueue(new MyCallBack<String>() {
                     @Override
                     public void _onResponse(Response<String> response) {
                         try {
                             if (response.raw().code() == 200) {
-                                Gson gson = new Gson();
-                                CuttingToolBind cuttingToolBind = gson.fromJson(response.body(), CuttingToolBind.class);
+                                FastQueryVO fastQueryVO = mapper.readValue(response.body(), FastQueryVO.class);
 
-                                if (cuttingToolBind != null) {
-                                    // 添加材料刀数据
-                                    addDataForCailiao();
-
-                                    // 添加合成刀数据
-                                    addDataForHechengdao();
-
-                                    //添加设备数据
-                                    addDataForEquipment();
-
-                                    // 添加人员数据
-                                    addDataForpersonnel();
+                                if (fastQueryVO != null) {
+                                    Message message = new Message();
+                                    message.obj = fastQueryVO;
+                                    //输入授权和扫描授权的handler
+                                    quicQkueryHandler.sendMessage(message);
                                 } else {
                                     runOnUiThread(new Runnable() {
                                         @Override
@@ -237,7 +239,14 @@ public class C01S024_001Activity extends CommonActivity {
                                 }
                             });
                         } finally {
-
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (null != loading && loading.isShowing()) {
+                                        loading.dismiss();
+                                    }
+                                }
+                            });
                         }
                     }
 
@@ -259,68 +268,103 @@ public class C01S024_001Activity extends CommonActivity {
         }
     }
 
+    //显示根据标签查询结果
+    @SuppressLint("HandlerLeak")
+    Handler quicQkueryHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            FastQueryVO fastQueryVO = (FastQueryVO) msg.obj;
+
+            if (fastQueryVO.getCuttingToolBind() != null) {
+                // 添加材料刀数据
+                addDataForCailiao(fastQueryVO.getCuttingToolBind());
+            }
+
+            if (fastQueryVO.getSynthesisCuttingToolBind() != null) {
+                // 添加合成刀数据
+                addDataForHechengdao(fastQueryVO.getSynthesisCuttingToolBind());
+            }
+
+            if (fastQueryVO.getEquipment() != null) {
+                //添加设备数据
+                addDataForEquipment(fastQueryVO.getEquipment());
+            }
+
+            if (fastQueryVO.getAuthCustomer() != null) {
+                // 添加人员数据
+                addDataForpersonnel(fastQueryVO.getAuthCustomer());
+            }
+        }
+    };
+
     /**
      * 添加材料刀数据
      */
-    private void addDataForCailiao() {
+    private void addDataForCailiao(CuttingToolBind cuttingToolBind) {
         hechengInfo.setVisibility(View.GONE);
         equipmentInfo.setVisibility(View.GONE);
         personnelInfo.setVisibility(View.GONE);
         cailiaoInfo.setVisibility(View.VISIBLE);
 
+        tvCailiaoInfoMaterialNumber.setText(cuttingToolBind.getCuttingTool().getBusinessCode());//材料号
+        tvCailiaoInfoFinalExecution.setText(cuttingToolBind.getRfidContainer().getOperatorName());//最后执行操作
 
-        tvCailiaoInfoMaterialNumber.setText("材料号");//材料号
-        tvCailiaoInfoFinalExecution.setText("最后执行操作");//最后执行操作
-        tvCailiaoInfoOperator.setText("操作者");//操作者
-        tvCailiaoInfoOperationTime.setText("操作时间");//操作时间
-        tvCailiaoInfoGrindingTimes.setText("修磨次数");//修磨次数
-        tvCailiaoInfoCumulativeAmountOfProcessing.setText("累计加工量");//累计加工量
+        tvCailiaoInfoOperator.setText("");//操作者
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy年MM月dd日");
+        String date = df.format(cuttingToolBind.getRfidContainer().getOperatorTime());
+        tvCailiaoInfoOperationTime.setText(date);//操作时间
+
+        tvCailiaoInfoGrindingTimes.setText(cuttingToolBind.getSharpenTimes());//修磨次数
+        tvCailiaoInfoCumulativeAmountOfProcessing.setText("");//累计加工量
     }
 
     /**
      * 添加合成刀数据
      */
-    private void addDataForHechengdao() {
+    private void addDataForHechengdao(SynthesisCuttingToolBind synthesisCuttingToolBind) {
         cailiaoInfo.setVisibility(View.GONE);
         equipmentInfo.setVisibility(View.GONE);
         personnelInfo.setVisibility(View.GONE);
         hechengInfo.setVisibility(View.VISIBLE);
 
 
-        tvHechengInfoSyntheticToolCode.setText("合成刀具编码");//合成刀具编码
-        tvHechengInfoFinalExecution.setText("最后执行操作");//最后执行操作
-        tvHechengInfoOperator.setText("操作者");//操作者
-        tvHechengInfoOperationTime.setText("操作时间");//操作时间
-        tvHechengInfoGrindingTimes.setText("修磨次数");//修磨次数
-        tvHechengInfoCumulativeAmountOfProcessing.setText("累计加工量");//累计加工量
+        tvHechengInfoSyntheticToolCode.setText(synthesisCuttingToolBind.getSynthesisCuttingTool().getSynthesisCode());//合成刀具编码
+        tvHechengInfoFinalExecution.setText(synthesisCuttingToolBind.getRfidContainer().getOperatorName());//最后执行操作
+        tvHechengInfoOperator.setText("");//操作者
+        SimpleDateFormat df = new SimpleDateFormat("yyyy年MM月dd日");
+        String date = df.format(synthesisCuttingToolBind.getRfidContainer().getOperatorTime());
+        tvHechengInfoOperationTime.setText(date);//操作时间
+        tvHechengInfoGrindingTimes.setText("");//修磨次数
+        tvHechengInfoCumulativeAmountOfProcessing.setText("");//累计加工量
     }
 
     /**
      * 添加设备数据
      */
-    private void addDataForEquipment() {
+    private void addDataForEquipment(ProductLineEquipment equipment) {
         cailiaoInfo.setVisibility(View.GONE);
         hechengInfo.setVisibility(View.GONE);
         personnelInfo.setVisibility(View.GONE);
         equipmentInfo.setVisibility(View.VISIBLE);
 
 
-        tvEquipmentInfoEquipmentName.setText("设备名称");//设备名称
+        tvEquipmentInfoEquipmentName.setText(equipment.getName());//设备名称
     }
 
     /**
      * 添加人员数据
      */
-    private void addDataForpersonnel() {
+    private void addDataForpersonnel(AuthCustomer authCustomer) {
         cailiaoInfo.setVisibility(View.GONE);
         hechengInfo.setVisibility(View.GONE);
         equipmentInfo.setVisibility(View.GONE);
         personnelInfo.setVisibility(View.VISIBLE);
 
 
-        tvPersonnelInfoEmployeeNumber.setText("员工号");//员工号
-        tvPersonnelInfoRealName.setText("真实姓名");//真实姓名
-        tvPersonnelInfoDepartment.setText("部门");//部门
+        tvPersonnelInfoEmployeeNumber.setText(authCustomer.getEmployeeCode());//员工号
+        tvPersonnelInfoRealName.setText(authCustomer.getName());//真实姓名
+        tvPersonnelInfoDepartment.setText(authCustomer.getAuthDepartment().getName());//部门
     }
 
 
