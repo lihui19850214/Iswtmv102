6dc7806062c26e65f27c8b4217df154623fe1fdb
李辉 <>
2018-05-27 13:33:08 +0800

    添加查询等待圈

--- src/com/icomp/Iswtmv10/v01c00/c00s000/C00S000_003Activity.java
+++ src/com/icomp/Iswtmv10/v01c00/c00s000/C00S000_003Activity.java
@@ -100,7 +100,6 @@ public class C00S000_003Activity extends CommonActivity {
         generateMenu();
 
         loading.show();
-        loading.dismiss();
         initThread = new InitThread();
         initThread.start();
     }
