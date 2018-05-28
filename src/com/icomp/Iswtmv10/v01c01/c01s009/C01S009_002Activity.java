0d62b60fc152149bdf7f35b7dcf4bf48e179e776
李辉 <>
2018-05-28 14:44:57 +0800

    修改操作赋值

--- src/com/icomp/Iswtmv10/v01c01/c01s009/C01S009_002Activity.java
+++ src/com/icomp/Iswtmv10/v01c01/c01s009/C01S009_002Activity.java
@@ -683,8 +683,8 @@ public class C01S009_002Activity extends CommonActivity {
                 impowerRecorder.setOperatorUserName(authCustomer.getName());//操作者姓名
                 impowerRecorder.setImpowerUser(authorizationList.get(0).getCode());//授权人code
                 impowerRecorder.setImpowerUserName(authorizationList.get(0).getName());//授权人名称
-                impowerRecorder.setOperatorKey(OperationEnum.SynthesisCuttingTool_Exchange.getKey().toString());//操作key
-                impowerRecorder.setOperatorValue(OperationEnum.SynthesisCuttingTool_Exchange.getName());//操作者code
+                impowerRecorder.setOperatorKey(OperationEnum.SynthesisCuttingTool_Config.getKey().toString());//操作key
+                impowerRecorder.setOperatorValue(OperationEnum.SynthesisCuttingTool_Config.getName());//操作者code
 
                 headsMap.put("impower", mapper.writeValueAsString(impowerRecorder));
             } catch (JsonProcessingException e) {
