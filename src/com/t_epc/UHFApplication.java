91cbb80442466b3fc03773aedaf06f97314aa6a9
李辉 <>
2018-06-04 19:51:22 +0800

    全局异常处理提示，保持不崩溃

--- src/com/t_epc/UHFApplication.java
+++ src/com/t_epc/UHFApplication.java
@@ -7,6 +7,11 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothSocket;
 
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+import com.icomp.Iswtmv10.R;
 import com.icomp.wsdl.common.UrlBase;
 import com.t_epc.reader.server.ReaderHelper;
 
@@ -37,6 +42,64 @@ public class UHFApplication extends Application {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        // ***************************** 捕获主线程异常，保持 APP 不崩溃 *****************************
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Looper.loop();
+                    } catch (Exception e) {
+//                        // 捕获到异常: main 在 android.app.ActivityThread
+//                        Log.e("捕获到异常", Thread.currentThread().getName() + " 在 " + e.getStackTrace()[0].getClassName());
+//                        // 捕获到异常: com.icomp.Iswtmv10.v01c01.c01s002.c01s002_002Activity
+//                        Log.e("捕获到异常", e.getCause().getStackTrace()[0].getClassName());
+//                        // 捕获到异常: c01s002_002Activity.java
+//                        Log.e("捕获到异常", e.getCause().getStackTrace()[0].getFileName());
+//                        // 捕获到异常: onCreate
+//                        Log.e("捕获到异常", e.getCause().getStackTrace()[0].getMethodName());
+//                        // 捕获到异常: 63
+//                        Log.e("捕获到异常", e.getCause().getStackTrace()[0].getLineNumber()+"");
+
+                        Toast.makeText(getApplicationContext(), getString(R.string.dataError), Toast.LENGTH_SHORT).show();
+                    }
+
+//                    catch (NullPointerException e) {
+//                        Toast.makeText(getApplicationContext(), "很抱歉,程序出现异常,即将退出.", Toast.LENGTH_SHORT).show();
+//                    } catch (Exception e){
+//                        //使用Toast来显示异常信息
+//                        new Thread() {
+//                            @Override
+//                            public void run() {
+//                                Looper.prepare();
+//                                Toast.makeText(getApplicationContext(), "----很抱歉,程序出现异常,即将退出----.", Toast.LENGTH_SHORT).show();
+//                                Looper.loop();
+//                            }
+//                        }.start();
+//                        try {
+//                            Thread.sleep(3000);
+//                        } catch (InterruptedException e1) {
+//
+//                        }
+//                        //退出程序
+//                        android.os.Process.killProcess(android.os.Process.myPid());
+//                        System.exit(1);
+//                    }
+                }
+            }
+        });
+
+        // ***************************** 捕获子线程异常，保持 APP 不崩溃 *****************************
+        // 所有线程异常拦截，由于主线程的异常都被 catch 住了，所以下面的代码拦截到的都是子线程的异常
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                Looper.prepare();
+                Toast.makeText(getApplicationContext(), getString(R.string.dataError), Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }
+        });
 
 		/*CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(getApplicationContext());*/
