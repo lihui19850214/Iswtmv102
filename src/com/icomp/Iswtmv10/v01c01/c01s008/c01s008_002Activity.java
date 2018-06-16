d1df5a00086a2d76cb26dc5b7af3612a7ac604ff
李辉 <>
2018-06-17 01:22:08 +0800

    提一版编译不报错的代码

--- src/com/icomp/Iswtmv10/v01c01/c01s008/c01s008_002Activity.java
+++ src/com/icomp/Iswtmv10/v01c01/c01s008/c01s008_002Activity.java
@@ -610,7 +610,7 @@ public class c01s008_002Activity extends CommonActivity {
 
                     // ------------ 授权信息 ------------
                     impowerRecorder.setToolCode(synthesisCuttingToolBind.getSynthesisCuttingTool().getSynthesisCode());// 合成刀编码
-                    impowerRecorder.setRfidLasercode(synthesisCuttingToolBindRFID);// rfid标签
+                    impowerRecorder.setRfidLasercode(authCustomer.getRfidContainer().getLaserCode());// rfid标签
                     impowerRecorder.setOperatorUserCode(customer.getCode());//操作者code
                     impowerRecorder.setImpowerUser(authCustomer.getCode());//授权人code
                     impowerRecorder.setOperatorKey(OperationEnum.SynthesisCuttingTool_UnConfig.getKey().toString());//操作key
