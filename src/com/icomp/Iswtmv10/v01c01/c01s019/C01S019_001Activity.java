33ee56685885e8f0581c7a4727961f3b19dd6c87
李辉 <>
2018-06-18 02:07:35 +0800

    提一版编译不报错的代码

--- src/com/icomp/Iswtmv10/v01c01/c01s019/C01S019_001Activity.java
+++ src/com/icomp/Iswtmv10/v01c01/c01s019/C01S019_001Activity.java
@@ -421,9 +421,10 @@ public class C01S019_001Activity extends CommonActivity {
                                 for (CuttingTool ct : cuttingToolList) {
                                     // dj("1","刀具"),fj("2","辅具"),pt("3","配套"),other("9","其他");
                                     if (CuttingToolTypeEnum.dj.getKey().equals(ct.getType())) {
-                                        cuttingToolListTemp.add(ct);
-//                                        // griding_zt("1","可刃磨钻头"),griding_dp("2","可刃磨刀片"),single_use_dp("3","一次性刀片"),other("9","其他");
-//                                        if (CuttingToolConsumeTypeEnum.griding_zt.getKey().equals(ct.getConsumeType())) {}
+                                        // griding_zt("1","可刃磨钻头"),griding_dp("2","可刃磨刀片"),single_use_dp("3","一次性刀片"),other("9","其他");
+                                        if (!CuttingToolConsumeTypeEnum.griding_zt.getKey().equals(ct.getConsumeType())) {
+                                            cuttingToolListTemp.add(ct);
+                                        }
                                     }
                                 }
 
