ac596c9053e2f2166b1e4122f9281039637bc114
李辉 <>
2018-06-27 22:06:40 +0800

    增加扫描响声

--- src/com/icomp/Iswtmv10/v01c01/c01s024/C01S024_001Activity.java
+++ src/com/icomp/Iswtmv10/v01c01/c01s024/C01S024_001Activity.java
@@ -9,6 +9,8 @@ import android.widget.*;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.apiclient.constants.EquipmentTypeEnum;
+import com.apiclient.constants.GrindingEnum;
 import com.apiclient.pojo.AuthCustomer;
 import com.apiclient.pojo.CuttingToolBind;
 import com.apiclient.pojo.ProductLineEquipment;
@@ -34,58 +36,63 @@ public class C01S024_001Activity extends CommonActivity {
 
     @BindView(R.id.cailiaoInfo)
     LinearLayout cailiaoInfo;
-    @BindView(R.id.tv_cailiaoInfo_materialNumber)
+
+    @BindView(R.id.tv_cailiaoInfo_materialNumber)//物料号
     TextView tvCailiaoInfoMaterialNumber;
-    @BindView(R.id.tv_cailiaoInfo_finalExecution)
+    @BindView(R.id.tv_cailiaoInfo_bladeCode)//刀身码
+    TextView tvCailiaoInfoBladeCode;
+    @BindView(R.id.tv_cailiaoInfo_finalExecution)//最后操作
     TextView tvCailiaoInfoFinalExecution;
-    @BindView(R.id.tv_cailiaoInfo_operator)
+    @BindView(R.id.tv_cailiaoInfo_operator)//操作者
     TextView tvCailiaoInfoOperator;
-    @BindView(R.id.tv_cailiaoInfo_operationTime)
+    @BindView(R.id.tv_cailiaoInfo_operationTime)//操作事件
     TextView tvCailiaoInfoOperationTime;
-    @BindView(R.id.tv_cailiaoInfo_grindingTimes)
-    TextView tvCailiaoInfoGrindingTimes;
-    @BindView(R.id.tv_cailiaoInfo_cumulativeAmountOfProcessing)
+    @BindView(R.id.tv_cailiaoInfo_cumulativeAmountOfProcessing)//累计加工
     TextView tvCailiaoInfoCumulativeAmountOfProcessing;
+    @BindView(R.id.tv_cailiaoInfo_grinding_method)//刃磨方式
+    TextView tvCailiaoInfoGrindingMethod;
 
 
     @BindView(R.id.hechengInfo)
     LinearLayout hechengInfo;
-    @BindView(R.id.tv_hechengInfo_syntheticToolCode)
+
+    @BindView(R.id.tv_hechengInfo_syntheticToolCode)//合成刀
     TextView tvHechengInfoSyntheticToolCode;
-    @BindView(R.id.tv_hechengInfo_finalExecution)
+    @BindView(R.id.tv_hechengInfo_finalExecution)//最后操作
     TextView tvHechengInfoFinalExecution;
-    @BindView(R.id.tv_hechengInfo_operator)
+    @BindView(R.id.tv_hechengInfo_operator)//操作者
     TextView tvHechengInfoOperator;
-    @BindView(R.id.tv_hechengInfo_operationTime)
+    @BindView(R.id.tv_hechengInfo_operationTime)//操作时间
     TextView tvHechengInfoOperationTime;
-    @BindView(R.id.tv_hechengInfo_grindingTimes)
-    TextView tvHechengInfoGrindingTimes;
-    @BindView(R.id.tv_hechengInfo_cumulativeAmountOfProcessing)
+    @BindView(R.id.tv_hechengInfo_cumulativeAmountOfProcessing)//累计加工
     TextView tvHechengInfoCumulativeAmountOfProcessing;
 
 
     @BindView(R.id.equipmentInfo)
     LinearLayout equipmentInfo;
-    @BindView(R.id.tv_equipmentInfo_equipmentName)
+
+    @BindView(R.id.tv_equipmentInfo_equipmentName)//设备名称
     TextView tvEquipmentInfoEquipmentName;
+    @BindView(R.id.tv_equipmentInfo_equipmentType)//设备类型
+    TextView tvEquipmentInfoEquipmentType;
 
 
     @BindView(R.id.personnelInfo)
     LinearLayout personnelInfo;
-    @BindView(R.id.tv_personnelInfo_employeeNumber)
+
+    @BindView(R.id.tv_personnelInfo_employeeNumber)//员工号
     TextView tvPersonnelInfoEmployeeNumber;
-    @BindView(R.id.tv_personnelInfo_realName)
+    @BindView(R.id.tv_personnelInfo_realName)//真实姓名
     TextView tvPersonnelInfoRealName;
-    @BindView(R.id.tv_personnelInfo_department)
+    @BindView(R.id.tv_personnelInfo_department)//部门
     TextView tvPersonnelInfoDepartment;
+    @BindView(R.id.tv_personnelInfo_job)//职务
+    TextView tvPersonnelInfoJob;
 
     @BindView(R.id.btnScan)
     Button btnScan;
     @BindView(R.id.btnCancel)
     Button btnCancel;
-
-    //扫描线程
-    private scanThread scanThread;
 
     //调用接口
     private Retrofit retrofit;
@@ -114,6 +121,9 @@ public class C01S024_001Activity extends CommonActivity {
         //点击扫描按钮的方法
         scan();
     }
+
+    //扫描线程
+    private scanThread scanThread;
 
     //点击扫描按钮的方法
     private void scan() {
@@ -271,11 +281,11 @@ public class C01S024_001Activity extends CommonActivity {
                 //添加设备数据
                 addDataForEquipment(rfidQueryVO.getEquipment());
             }
-            // TODO 新接口没有人员信息
-//            if (rfidQueryVO.getAuthCustomer() != null) {
-//                // 添加人员数据
-//                addDataForpersonnel(rfidQueryVO.getAuthCustomer());
-//            }
+
+            if (rfidQueryVO.getAuthCustomer() != null) {
+                // 添加人员数据
+                addDataForpersonnel(rfidQueryVO.getAuthCustomer());
+            }
         }
     };
 
@@ -288,9 +298,14 @@ public class C01S024_001Activity extends CommonActivity {
         personnelInfo.setVisibility(View.GONE);
         cailiaoInfo.setVisibility(View.VISIBLE);
 
-        tvCailiaoInfoMaterialNumber.setText(cuttingToolBind.getCuttingTool().getBusinessCode());//材料号
-        tvCailiaoInfoFinalExecution.setText(cuttingToolBind.getRfidContainer().getPrevOperation());//最后执行操作
 
+        tvCailiaoInfoMaterialNumber.setText(cuttingToolBind.getCuttingTool().getBusinessCode());//物料号
+        String bladeCode = cuttingToolBind.getBladeCode();
+        if (bladeCode != null && bladeCode.indexOf("-") > 0) {
+            tvCailiaoInfoBladeCode.setText(bladeCode.split("-")[1]);//刀身码
+        }
+
+        tvCailiaoInfoFinalExecution.setText(cuttingToolBind.getRfidContainer().getPrevOperation());//最后操作
         tvCailiaoInfoOperator.setText(cuttingToolBind.getRfidContainer().getOperatorName());//操作者
 
         try {
@@ -301,10 +316,19 @@ public class C01S024_001Activity extends CommonActivity {
             e.printStackTrace();
         }
 
-        tvCailiaoInfoGrindingTimes.setText(cuttingToolBind.getSharpenTimes()+"");//修磨次数
         if (cuttingToolBind.getProcessingCount() != null) {
             tvCailiaoInfoCumulativeAmountOfProcessing.setText(cuttingToolBind.getProcessingCount() + "");//累计加工量
         }
+
+        String grinding = "";
+        if (GrindingEnum.inside.getKey().equals(cuttingToolBind.getCuttingTool().getGrinding())) {
+            grinding = GrindingEnum.inside.getName();
+        } else if (GrindingEnum.outside.getKey().equals(cuttingToolBind.getCuttingTool().getGrinding())) {
+            grinding = GrindingEnum.outside.getName();
+        } else if (GrindingEnum.outside_tuceng.getKey().equals(cuttingToolBind.getCuttingTool().getGrinding())) {
+            grinding = GrindingEnum.outside_tuceng.getName();
+        }
+        tvCailiaoInfoGrindingMethod.setText(grinding);//修磨方式
     }
 
     /**
@@ -317,8 +341,8 @@ public class C01S024_001Activity extends CommonActivity {
         hechengInfo.setVisibility(View.VISIBLE);
 
 
-        tvHechengInfoSyntheticToolCode.setText(synthesisCuttingToolBind.getSynthesisCuttingTool().getSynthesisCode());//合成刀具编码
-        tvHechengInfoFinalExecution.setText(synthesisCuttingToolBind.getRfidContainer().getPrevOperation());//最后执行操作
+        tvHechengInfoSyntheticToolCode.setText(synthesisCuttingToolBind.getSynthesisCuttingTool().getSynthesisCode());//合成刀
+        tvHechengInfoFinalExecution.setText(synthesisCuttingToolBind.getRfidContainer().getPrevOperation());//最后操作
         tvHechengInfoOperator.setText(synthesisCuttingToolBind.getRfidContainer().getOperatorName());//操作者
 
         try {
@@ -328,23 +352,32 @@ public class C01S024_001Activity extends CommonActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        tvHechengInfoGrindingTimes.setText(synthesisCuttingToolBind.);//修磨次数
+
         if (synthesisCuttingToolBind.getProcessingCount() != null) {
-            tvHechengInfoCumulativeAmountOfProcessing.setText(synthesisCuttingToolBind.getProcessingCount() + "");//累计加工量
+            tvHechengInfoCumulativeAmountOfProcessing.setText(synthesisCuttingToolBind.getProcessingCount() + "");//累计加工
         }
     }
 
     /**
      * 添加设备数据
      */
-    private void addDataForEquipment(ProductLineEquipment equipment) {
+    private void addDataForEquipment(ProductLineEquipment productLineEquipment) {
         cailiaoInfo.setVisibility(View.GONE);
         hechengInfo.setVisibility(View.GONE);
         personnelInfo.setVisibility(View.GONE);
         equipmentInfo.setVisibility(View.VISIBLE);
 
 
-        tvEquipmentInfoEquipmentName.setText(equipment.getName());//设备名称
+        tvEquipmentInfoEquipmentName.setText(productLineEquipment.getName());//设备名称
+
+        String type = "";
+        if (EquipmentTypeEnum.Processing.getKey().equals(productLineEquipment.getType())) {
+            type = EquipmentTypeEnum.Processing.getName();
+        } else if (EquipmentTypeEnum.Grinding.getKey().equals(productLineEquipment.getType())) {
+            type = EquipmentTypeEnum.Grinding.getName();
+        }
+
+        tvEquipmentInfoEquipmentType.setText(type);//设备类型
     }
 
     /**
@@ -360,6 +393,7 @@ public class C01S024_001Activity extends CommonActivity {
         tvPersonnelInfoEmployeeNumber.setText(authCustomer.getEmployeeCode());//员工号
         tvPersonnelInfoRealName.setText(authCustomer.getName());//真实姓名
         tvPersonnelInfoDepartment.setText(authCustomer.getAuthDepartment().getName());//部门
+        tvPersonnelInfoJob.setText(authCustomer.getAuthPosition().getName());//职务
     }
 
 
