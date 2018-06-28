371b52d0b2321a4a3a98368ef3ce4fb64165d028
李辉 <>
2018-06-28 11:34:48 +0800

    添加属性

--- src/com/apiclient/vo/RFIDQueryVO.java
+++ src/com/apiclient/vo/RFIDQueryVO.java
@@ -1,13 +1,23 @@
 package com.apiclient.vo;
 
+
+import com.apiclient.pojo.AuthCustomer;
 import com.apiclient.pojo.CuttingToolBind;
 import com.apiclient.pojo.ProductLineEquipment;
 import com.apiclient.pojo.SynthesisCuttingToolBind;
+
+import java.util.List;
 
 /**
  * Created by logan on 2018/5/7.
  */
 public class RFIDQueryVO {
+
+    private AuthCustomer loginUser;
+
+    private AuthCustomer authCustomer;
+
+    private List<RfidContainerVO> rfidContainerVOList;
 
     private String rfidCode;
 
@@ -16,6 +26,30 @@ public class RFIDQueryVO {
     private ProductLineEquipment equipment;
 
     private SynthesisCuttingToolBind synthesisCuttingToolBind;
+
+    public AuthCustomer getAuthCustomer() {
+        return authCustomer;
+    }
+
+    public void setAuthCustomer(AuthCustomer authCustomer) {
+        this.authCustomer = authCustomer;
+    }
+
+    public AuthCustomer getLoginUser() {
+        return loginUser;
+    }
+
+    public void setLoginUser(AuthCustomer loginUser) {
+        this.loginUser = loginUser;
+    }
+
+    public List<RfidContainerVO> getRfidContainerVOList() {
+        return rfidContainerVOList;
+    }
+
+    public void setRfidContainerVOList(List<RfidContainerVO> rfidContainerVOList) {
+        this.rfidContainerVOList = rfidContainerVOList;
+    }
 
     public String getRfidCode() {
         return rfidCode;
