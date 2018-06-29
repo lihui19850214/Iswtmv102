2b4e0adc53cbd2fd4c0523fa1407f3979282a343
李辉 <>
2018-06-29 12:10:33 +0800

    报废VO

--- src/com/apiclient/vo/ScrapVO.java
+++ src/com/apiclient/vo/ScrapVO.java
@@ -1,31 +1,71 @@
 package com.apiclient.vo;
 
-import com.apiclient.pojo.AuthCustomer;
 
-import java.util.List;
+import com.apiclient.pojo.CuttingTool;
+import com.apiclient.pojo.CuttingToolBind;
 
 /**
  * Created by logan on 2018/5/12.
  */
 public class ScrapVO {
 
-    private List<SharpenVO> sharpenVOS;
+    private CuttingToolVO cuttingToolVO;
 
-    private AuthCustomer authCustomer;
+    private CuttingToolBindVO cuttingToolBindVO;
 
-    public List<SharpenVO> getSharpenVOS() {
-        return sharpenVOS;
+    private CuttingTool cuttingTool;
+
+    private CuttingToolBind cuttingToolBind;
+
+    private Integer count;
+
+    private String status;
+
+    public String getStatus() {
+        return status;
     }
 
-    public void setSharpenVOS(List<SharpenVO> sharpenVOS) {
-        this.sharpenVOS = sharpenVOS;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    public AuthCustomer getAuthCustomer() {
-        return authCustomer;
+    public CuttingTool getCuttingTool() {
+        return cuttingTool;
     }
 
-    public void setAuthCustomer(AuthCustomer authCustomer) {
-        this.authCustomer = authCustomer;
+    public void setCuttingTool(CuttingTool cuttingTool) {
+        this.cuttingTool = cuttingTool;
+    }
+
+    public CuttingToolBind getCuttingToolBind() {
+        return cuttingToolBind;
+    }
+
+    public void setCuttingToolBind(CuttingToolBind cuttingToolBind) {
+        this.cuttingToolBind = cuttingToolBind;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public CuttingToolVO getCuttingToolVO() {
+        return cuttingToolVO;
+    }
+
+    public void setCuttingToolVO(CuttingToolVO cuttingToolVO) {
+        this.cuttingToolVO = cuttingToolVO;
+    }
+
+    public CuttingToolBindVO getCuttingToolBindVO() {
+        return cuttingToolBindVO;
+    }
+
+    public void setCuttingToolBindVO(CuttingToolBindVO cuttingToolBindVO) {
+        this.cuttingToolBindVO = cuttingToolBindVO;
     }
 }
