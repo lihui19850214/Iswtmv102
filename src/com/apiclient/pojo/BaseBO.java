22ff7f8d2f98a4da5cb742334d15984d78daa72d
李辉 <>
2018-06-29 12:09:54 +0800

    报废PO

---
+++ src/com/apiclient/pojo/BaseBO.java
@@ -0,0 +1,17 @@
+package com.apiclient.pojo;
+
+/**
+ * Created by logan on 2018/6/12.
+ */
+public class BaseBO {
+
+    private AuthCustomer loginUser;
+
+    public AuthCustomer getLoginUser() {
+        return loginUser;
+    }
+
+    public void setLoginUser(AuthCustomer loginUser) {
+        this.loginUser = loginUser;
+    }
+}
