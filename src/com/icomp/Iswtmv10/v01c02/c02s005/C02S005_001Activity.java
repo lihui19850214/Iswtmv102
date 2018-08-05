139f6b91639b1600c6380a217c4fde97b24f5229
李辉 <>
2018-08-06 02:18:12 +0800

    射频设置业务

--- src/com/icomp/Iswtmv10/v01c02/c02s005/C02S005_001Activity.java
+++ src/com/icomp/Iswtmv10/v01c02/c02s005/C02S005_001Activity.java
@@ -1,8 +1,8 @@
 package com.icomp.Iswtmv10.v01c02.c02s005;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -10,59 +10,27 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.icomp.Iswtmv10.R;
 import com.icomp.common.activity.CommonActivity;
-import com.icomp.common.utils.RfidRead;
-import com.umeng.analytics.MobclickAgent;
 
 public class C02S005_001Activity extends CommonActivity {
-    private int paramInt = 0;
-    byte frequency = '0';
 
-    /**
-     * F1按键
-     *
-     * @return true:事件继续执行,false:事件停止
-     */
-    @Override
-    public boolean keycodeMenu() {
-        finish();
-        return false;
-    }
+    @BindView(R.id.seekBar1)
+    SeekBar seekBar1;
+    @BindView(R.id.btnSave)
+    Button btnSave;
+    @BindView(R.id.btnReturn)
+    Button btnReturn;
+    @BindView(R.id.tv_power)
+    TextView tvPower;
 
-    /**
-     * 取消按钮处理
-     */
-    @Override
-    public void appCancel() {
-        finish();
-    }
+    private int powerValue = 5;
 
-    //取消按钮处理--跳转到系统菜单页面
-    public void btnCancel(View view) {
-        finish();
-    }
+    private int arrPow = 25; //输出功率，分为最大30和24
 
-    /**
-     * Enter按键
-     *
-     * @return true:事件继续执行,false:事件停止
-     */
-    public boolean keycodeEnter() {
-        if (paramInt == 0) {
-            paramInt = frequency;
-        }
-        // 取得用户已保存的默认语言
-        SharedPreferences sharedPreferences = getSharedPreferences("langVaue", CommonActivity.MODE_APPEND);
-        // 保存语言信息
-        sharedPreferences = getSharedPreferences("rfidFrequency", Context.MODE_APPEND);
-        SharedPreferences.Editor editor = sharedPreferences.edit();// 获取编辑器
-        editor.putString("paramInt", paramInt + "");
-        editor.commit();// 提交修改
-        createAlertDialog(this, getString(R.string.saveSuccess), Toast.LENGTH_SHORT);
-        return false;
-    }
+    private Handler mHandler = new Handler();
 
     /**
      * Called when the activity is first created.
@@ -74,66 +42,108 @@ public class C02S005_001Activity extends CommonActivity {
         setContentView(R.layout.c02s005_001activity);
         ButterKnife.bind(this);
 
+        String ver = rfidWithUHF.getHardwareType();
 
-//        RfidRead.initRfid(this);
-//        final SeekBar seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
-//        // 取得读头模块信号强度
-//        //	AppContext.getJni().WIrUHFGetPower(uPower);
-//        SharedPreferences preferences = getSharedPreferences("rfidFrequency", CommonActivity.MODE_APPEND);
-//        String paramInit = preferences.getString("paramInt", "18");
-//        frequency = (byte) Integer.parseInt(paramInit);
-//        seekBar1.setMax(30);
-//        seekBar1.setProgress(Integer.parseInt(paramInit));
-//        TextView textView1 = (TextView) C02S005_001Activity.this.findViewById(R.id.textView1);
-//        textView1.setText(getString(R.string.power) + ":" + String.valueOf(frequency) + "dBM");
-//        Button btnSave = (Button) findViewById(R.id.btnSave);
-//        btnSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View arg0) {
-//                keycodeEnter();
-//            }
-//        });
-//
-//        seekBar1.setOnSeekBarChangeListener(new OnSeekBarChangeListener() // 调音监听器
-//        {
-//            public void onProgressChanged(SeekBar arg0, int progress, boolean fromUser) {
-//                paramInt = progress;
-//                TextView textView1 = (TextView) C02S005_001Activity.this.findViewById(R.id.textView1);
-//                textView1.setText(getString(R.string.power) + ":" + progress + "dBM");
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
-//
-//        // 取得取消按钮
-//        Button cancel = (Button) findViewById(R.id.cancel);
-//        if (cancel != null) {
-//            cancel.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View arg0) {
-//                    appCancel();
-//                }
-//            });
-//        }
-//        // 取得返回按钮
-//        Button btnReturn = (Button) findViewById(R.id.btnReturn);
-//        if (btnReturn != null) {
-//            btnReturn.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View arg0) {
-//                    appCancel();
-//                }
-//            });
-//        }
+        if (ver != null && ver.contains("RLM")) {
+            arrPow = 19;
+        }
 
+        // 设置 seekBar 最大值
+        seekBar1.setMax(arrPow);
+
+        /*
+		开启子线程获取参数，Handler更新UI,防止打开卡顿
+		 */
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                getPower();
+            }
+        });
+
+//        getPower();
+
+        // 调音监听器
+        seekBar1.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar arg0, int progress, boolean fromUser) {
+                powerValue = progress + 5;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvPower.setText(String.valueOf(powerValue));
+                    }
+                });
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // 保存按钮事件
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                setPower();
+            }
+        });
+
+        // 返回按钮事件
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+
+    }
+
+    public void getPower() {
+        int iPower = rfidWithUHF.getPower();
+
+        //Log.i("C02S005_001Activity", "getPower() iPower=" + iPower);
+
+        if (iPower > -1) {
+            int tempProgress = iPower - 5;
+            final int progress;
+
+            if (tempProgress < 5) {
+                powerValue = 5;
+                progress = 0;
+            } else if (tempProgress > seekBar1.getMax()) {
+                powerValue = seekBar1.getMax() + 5;
+                progress = seekBar1.getMax();
+            } else {
+                powerValue = tempProgress + 5;
+                progress = tempProgress;
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvPower.setText(String.valueOf(powerValue));
+                    seekBar1.setProgress(progress);
+                }
+            });
+        } else {
+            createToast(getApplicationContext(), getString(R.string.uhf_msg_read_power_fail), Toast.LENGTH_SHORT);
+        }
+    }
+
+    public void setPower() {
+        //Log.i("C02S005_001Activity", "setPower() iPower=" + powerValue);
+
+        if (rfidWithUHF.setPower(powerValue)) {
+            createToast(getApplicationContext(), getString(R.string.uhf_msg_set_power_succ), Toast.LENGTH_SHORT);
+        } else {
+            createToast(getApplicationContext(), getString(R.string.uhf_msg_set_power_fail), Toast.LENGTH_SHORT);
+        }
     }
 
 }
